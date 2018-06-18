505d03269a0d919a5bdff0ea924c36726f8c92ff
Nicolas Lopez <nicolaslopezj@me.com>
2018-06-18 18:13:46 -0400

    file-manager work with url with spaces

--- packages/file-manager/src/File/resolvers/url.js
+++ packages/file-manager/src/File/resolvers/url.js
@@ -3,6 +3,6 @@ import {resolver} from '@orion-js/app'
 export default resolver({
   returns: String,
   async resolve(file, viewer) {
-    return `https://s3.amazonaws.com/${file.bucket}/${file.key}`
+    return `https://s3.amazonaws.com/${file.bucket}/${file.key.replace(/ /, '%20')}`
   }
 })
