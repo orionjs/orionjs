fdd02096e049cc7093c044790eb3c24686e0ee62
Nicolás López <nicolaslopezj@me.com>
2018-07-03 16:37:17 -0400

    update dependencies on example

--- examples/basic/app/services/http/home.js
+++ examples/basic/app/services/http/home.js
@@ -1,11 +1,5 @@
 import {route} from '@orion-js/app'
-import {sendEmail} from '@orion-js/mailing'
 
 route('/', async function() {
-  await sendEmail({
-    to: ['anemail@me.com', 'a2email@me.com'],
-    subject: 'testing',
-    text: 'Hello this is a text'
-  })
-  return 'hi'
+  return 'hello world'
 })
