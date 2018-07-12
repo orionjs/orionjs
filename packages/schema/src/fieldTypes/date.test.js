509944deba23e2d3bef197638c797fde65fd2c0c
Nicolás López <nicolaslopezj@me.com>
2018-07-12 13:05:10 -0400

    clean date types

--- packages/schema/src/fieldTypes/date.test.js
+++ packages/schema/src/fieldTypes/date.test.js
@@ -11,3 +11,29 @@ test('return an error when the value is incorrect', async () => {
 test('return no error when the value is correct', async () => {
   expect(date.validate(new Date())).toBeFalsy()
 })
+
+test('dont autoConvert if not specified', async () => {
+  const info = {options: {autoConvert: false}}
+  expect(date.clean('2018-07-21T16:00:00.000Z', info)).toBe('2018-07-21T16:00:00.000Z')
+})
+
+test('return the same type if invalid is passed', async () => {
+  const info = {options: {autoConvert: true}}
+  expect(date.clean([123], info)).toEqual([123])
+})
+
+test('clean string types', async () => {
+  const info = {options: {autoConvert: true}}
+  expect(date.clean('2018-07-21T16:00:00.000Z', info)).toEqual(new Date('2018-07-21T16:00:00.000Z'))
+})
+
+test('clean number types', async () => {
+  const info = {options: {autoConvert: true}}
+  expect(date.clean(1532188800000, info)).toEqual(new Date('2018-07-21T16:00:00.000Z'))
+})
+
+test('return the same value when invalid types are passed', async () => {
+  const info = {options: {autoConvert: true}}
+  expect(date.clean('Nicolás', info)).toBe('Nicolás')
+  expect(date.clean(1532188800000000000, info)).toBe(1532188800000000000)
+})
