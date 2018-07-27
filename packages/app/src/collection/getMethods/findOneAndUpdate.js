a14d7185c486afc009ee3c8104e823ad7aebf344
Nicolás López <nicolaslopezj@me.com>
2018-07-27 11:40:13 -0400

    change findone and update function name (internal)

--- packages/app/src/collection/getMethods/findOneAndUpdate.js
+++ packages/app/src/collection/getMethods/findOneAndUpdate.js
@@ -3,7 +3,7 @@ import validateModifier from './validateModifier'
 import cleanModifier from './cleanModifier'
 
 export default ({initItem, rawCollection, model, schema}) =>
-  async function findOne(...args) {
+  async function findOneAndUpdate(...args) {
     const selector = getSelector(args)
     let modifier = args[1]
     const options = args[2] || {}
