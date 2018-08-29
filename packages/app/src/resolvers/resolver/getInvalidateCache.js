8d9a25edf6be90b7484121d059bb1384eac44c4c
Nicolás López <nicolaslopezj@me.com>
2018-08-29 15:01:41 -0300

    delete log in resolver

--- packages/app/src/resolvers/resolver/getInvalidateCache.js
+++ packages/app/src/resolvers/resolver/getInvalidateCache.js
@@ -4,7 +4,6 @@ import getCacheKey from './getCacheKey'
 export default function(resolver) {
   return async function(params, parent) {
     const cacheKey = getCacheKey({parent, callParams: params, resolverId: resolver.resolverId})
-    console.log('will invalidate cache', params, parent, {cacheKey})
     await cache.invalidate(cacheKey)
   }
 }
