ca20ff7ff89cf3cace9282b55d673ac205105210
Nicolás López <nicolaslopezj@me.com>
2018-12-13 17:48:45 -0300

    allow passing no queries or mutations

--- packages/graphql/src/buildSchema/getMutation.js
+++ packages/graphql/src/buildSchema/getMutation.js
@@ -1,8 +1,10 @@
 import {GraphQLObjectType} from 'graphql'
 import getResolvers from './getResolvers'
+import isEmpty from 'lodash/isEmpty'
 
 export default async function({resolvers}) {
   const fields = await getResolvers({resolvers, mutation: true})
+  if (isEmpty(fields)) return null
   return new GraphQLObjectType({
     name: 'Mutation',
     fields
--- packages/graphql/src/buildSchema/getQuery.js
+++ packages/graphql/src/buildSchema/getQuery.js
@@ -1,8 +1,10 @@
 import {GraphQLObjectType} from 'graphql'
 import getResolvers from './getResolvers'
+import isEmpty from 'lodash/isEmpty'
 
 export default async function({resolvers}) {
   const fields = await getResolvers({resolvers, mutation: false})
+  if (isEmpty(fields)) return null
   return new GraphQLObjectType({
     name: 'Query',
     fields
