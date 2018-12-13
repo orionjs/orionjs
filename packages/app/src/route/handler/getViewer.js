3aaea30e429b2cf5c2b5b776f740cae01836e92e
Nicolás López <nicolaslopezj@me.com>
2018-12-13 17:46:02 -0300

    allow setting no viewer

--- packages/app/src/route/handler/getViewer.js
+++ packages/app/src/route/handler/getViewer.js
@@ -4,7 +4,7 @@ import UserError from '../../Errors/UserError'
 export default async function(params) {
   try {
     const getViewer = getGetViewer()
-    const viewer = params ? (await getViewer(params)) || {} : {}
+    const viewer = params && getViewer ? (await getViewer(params)) || {} : {}
     viewer.locale = viewer.locale || (params && params.headers['x-orion-locale']) || 'en'
     return viewer
   } catch (error) {
