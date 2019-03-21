a0542b4e9116a81d75d163e4ad177b5a17eb0622
Nicolás López <nicolaslopezj@me.com>
2019-03-21 15:35:33 -0300

    fix date scalar

--- packages/graphql/src/buildSchema/getType/DateScalar.js
+++ packages/graphql/src/buildSchema/getType/DateScalar.js
@@ -1,43 +1,9 @@
-import {GraphQLScalarType, GraphQLError, Kind} from 'graphql'
+import {GraphQLScalarType} from 'graphql'
+import {GraphQLDateTime} from 'graphql-iso-date'
 
 export default new GraphQLScalarType({
   name: 'Date',
-  serialize: function(value) {
-    if (!(value instanceof Date)) {
-      throw new Error('Field error: value is not an instance of Date')
-    }
-    if (isNaN(value.getTime())) {
-      throw new Error('Field error: value is an invalid Date')
-    }
-    return value.toJSON()
-  },
-  parseValue: function(value) {
-    const date = new Date(value)
-    if (isNaN(date.getTime())) {
-      throw new Error('Field error: value is an invalid Date')
-    }
-    return date
-  },
-  parseLiteral: function(ast) {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError(
-        'Query error: Can only parse strings to dates but got a: ' + ast.kind,
-        [ast]
-      )
-    }
-
-    const result = new Date(ast.value)
-    if (isNaN(result.getTime())) {
-      throw new GraphQLError('Query error: Invalid date', [ast])
-    }
-
-    if (ast.value !== result.toJSON()) {
-      throw new GraphQLError(
-        'Query error: Invalid date format, only accepts: YYYY-MM-DDTHH:MM:SS.SSSZ',
-        [ast]
-      )
-    }
-
-    return result
-  }
+  serialize: GraphQLDateTime._scalarConfig.serialize,
+  parseValue: GraphQLDateTime._scalarConfig.parseValue,
+  parseLiteral: GraphQLDateTime._scalarConfig.parseLiteral
 })
