92886f200de13a375d1a80b9297efa8eb91adb24
Nicolás López <nicolaslopezj@me.com>
2019-05-03 17:28:33 -0400

    fix message when resolver returns nil on array

--- packages/app/src/resolvers/resolver/getResolver/initResult.js
+++ packages/app/src/resolvers/resolver/getResolver/initResult.js
@@ -4,7 +4,9 @@ import isNil from 'lodash/isNil'
 export default function({returns, result}) {
   if (returns) {
     if (isArray(returns) && returns[0].__isModel) {
-      if (!isNil(result) && isArray(result)) {
+      if (isNil(result)) {
+        return result
+      } else if (isArray(result)) {
         return result.map(item => returns[0].initItem(item))
       } else {
         console.warn(`A resolver did not return an array when it should`, result)
