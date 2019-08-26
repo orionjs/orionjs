401c4b4d2efc5117173fd83bd0c3c20151b93c7e
Nicolás López <nicolaslopezj@me.com>
2019-08-26 16:44:32 -0400

    show compilation error before deleting dir

--- packages/cli/src/build/compile.js
+++ packages/cli/src/build/compile.js
@@ -12,13 +12,13 @@ export default async function(dirPath) {
     await Promise.all(files.map(file => compileFile(file, finalDirPath)))
     return true
   } catch (error) {
-    await execute(`rm -rf ${finalDirPath}`)
     console.log(colors.red(`=> Syntax error at ${error.message}`))
     if (error._babel) {
       console.log(error.codeFrame)
     } else {
       console.error(colors.red(error))
     }
+    await execute(`rm -rf ${finalDirPath}`)
     return false
   }
 }
