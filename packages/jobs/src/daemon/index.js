9ab6e59890a95233aa9d55a0aacf12184fb405fb
Nicolás López Jullian <nicolaslopezj@me.com>
2020-06-22 10:53:52 -0400

    expose job workers

--- packages/jobs/src/daemon/index.js
+++ packages/jobs/src/daemon/index.js
@@ -2,7 +2,9 @@ import runLoop from './runLoop'
 import range from 'lodash/range'
 import Worker from '../Worker'
 
-export default function({jobs, workersCount}) {
+export default function ({jobs, workersCount}) {
   const workers = range(workersCount).map(index => new Worker({index}))
   runLoop({jobs, workers})
+
+  global.jobWorkers = workers
 }
