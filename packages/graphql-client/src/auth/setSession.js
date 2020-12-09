be7522e864761e8daf15a0f374772f6e55789a72
Nicolás López Jullian <nicolaslopezj@me.com>
2020-12-09 17:18:33 -0300

    improve session

--- packages/graphql-client/src/auth/setSession.js
+++ packages/graphql-client/src/auth/setSession.js
@@ -3,10 +3,14 @@ import {getOptions} from '../options'
 export default async function (session, resetStore = true) {
   const {apolloClient, saveSession, wsClient} = getOptions()
 
+  if (resetStore) {
+    await apolloClient.clearStore()
+  }
+
   await saveSession(session)
 
   if (resetStore) {
-    await apolloClient.resetStore()
+    await apolloClient.reFetchObservableQueries()
   }
 
   if (wsClient) {
