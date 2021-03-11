4a7d72d16d7162d310e40233aad68d8d4b87700b
jaletelier <javier.letelier@getjusto.com>
2021-03-11 15:06:02 -0300

    graphql errorHandler hash

--- packages/graphql/src/errorHandler.js
+++ packages/graphql/src/errorHandler.js
@@ -1,4 +1,5 @@
 import {config, UserError} from '@orion-js/app'
+import crypto from 'crypto'
 
 export default function errorHandler(error, data) {
   const {logger} = config()
@@ -8,7 +9,13 @@ export default function errorHandler(error, data) {
   if (error && error.isOrionError) {
     logger.warn(message, error)
   } else {
+    const hash = crypto
+      .createHash('sha1')
+      .update(error.message, 'utf8')
+      .digest('hex')
+      .substring(0, 10)
+    error.hash = hash
     logger.error(message, error)
-    throw new UserError('SERVER_ERROR', 'unexpected server error')
+    throw new UserError('INTERNAL_SERVER_ERROR', 'unexpected server error', {hash})
   }
 }
