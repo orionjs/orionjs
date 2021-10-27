67135347ca8aecd8c3f24883a85d40096c194ccd
Nicolás López Jullian <nicolaslopezj@me.com>
2021-10-27 09:33:25 -0300

    orion errors

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -2,5 +2,17 @@ import sleep from './sleep'
 import hashObject from './hashObject'
 import generateId from './generateId'
 import createMap from './createMap'
+import {OrionError, OrionErrorInformation} from './Errors/OrionError'
+import PermissionsError from './Errors/PermissionsError'
+import UserError from './Errors/UserError'
 
-export {createMap, generateId, hashObject, sleep}
+export {
+  createMap,
+  generateId,
+  hashObject,
+  sleep,
+  OrionError,
+  PermissionsError,
+  UserError,
+  OrionErrorInformation
+}
