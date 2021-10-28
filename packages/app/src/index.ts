b0dd16befd7f0d7fd72c2a96ff29d5a4785a4013
Nicolás López Jullian <nicolaslopezj@me.com>
2021-10-28 12:25:42 -0300

    deletes finally app pacakge

--- packages/app/src/index.ts
+++
@@ -1,5 +0,0 @@
-import Model from './Model'
-import modelToSchema from './Model/modelToSchema'
-import {crudResolvers, paginatedResolver, tokenPaginatedResolver} from './resolvers'
-
-export {modelToSchema, Model, crudResolvers, tokenPaginatedResolver, paginatedResolver}
