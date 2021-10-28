fcf6d1bc153b5574bdaf7dc587b7434ddb64b518
Nicolás López Jullian <nicolaslopezj@me.com>
2021-10-28 12:01:38 -0300

    delete crud resolvers

--- packages/app/src/resolvers/crudResolvers/list.js
+++
@@ -1,9 +0,0 @@
-import paginatedResolver from '../paginatedResolver'
-
-export default ({name, collection, Model, paginatedOptions}) => {
-  return paginatedResolver({
-    returns: Model,
-    collection,
-    ...paginatedOptions
-  })
-}
--- packages/app/src/resolvers/index.ts
+++ packages/app/src/resolvers/index.ts
@@ -1,5 +1,4 @@
-import crudResolvers from './crudResolvers'
 import paginatedResolver from './paginatedResolver'
 import tokenPaginatedResolver from './tokenPaginatedResolver'
 
-export {crudResolvers, paginatedResolver, tokenPaginatedResolver}
+export {paginatedResolver, tokenPaginatedResolver}
