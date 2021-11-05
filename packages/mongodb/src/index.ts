ea4332a336a6d3277aacb5167f428e30276f0198
Nicolás López Jullian <nicolaslopezj@me.com>
2021-11-05 18:11:19 -0300

    dont export typed mongo from mongo

--- packages/mongodb/src/index.ts
+++ packages/mongodb/src/index.ts
@@ -2,8 +2,7 @@ import {connections} from './connect/connections'
 import {connectToDB} from './connect/connectToDB'
 import {connect} from './connect'
 import createCollection from './createCollection'
-import * as TypedModel from '@orion-js/typed-model'
 
-export {connect, connectToDB, connections, createCollection, TypedModel}
+export {connect, connectToDB, connections, createCollection}
 
 export * from './types'
