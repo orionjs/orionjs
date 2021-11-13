da3f65c7c0eed80941b03df48782beb3fea29734
Daniel Merrill <danielmerrill6@gmail.com>
2021-11-12 22:28:14 -0300

    [jobs]: export connection

--- packages/jobs/src/index.ts
+++ packages/jobs/src/index.ts
@@ -1,4 +1,13 @@
+import {getJobRetriesCollection} from './collections/getJobRetriesCollection'
+import {getJobUniquenessKeysCollection} from './collections/getJobUniquenessKeysCollection'
+
 export * from './operations' // init, start, stop
 export * from './job' // job(...) factory
 export * from './JobManager' // In case the user wants to access agenda instance
 export * from './types'
+export const getConnectionPromise = () => {
+  return Promise.all([
+    getJobRetriesCollection().connectionPromise,
+    getJobUniquenessKeysCollection().connectionPromise
+  ])
+}
