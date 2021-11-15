255672a6cf8e47bc811ad5739b905163fa006331
Nicolás López Jullian <nicolaslopezj@me.com>
2021-11-15 12:13:47 -0300

    update test jobs

--- packages/jobs/src/test/setup.ts
+++ packages/jobs/src/test/setup.ts
@@ -4,11 +4,9 @@ const url = `${global.__MONGO_URI__}jest`
 process.env.MONGO_URL = url
 const connection = connect(url)
 
-beforeAll(async () => {
-  await connection.connectionPromise
-})
-
 beforeEach(async () => {
+  await connection.connectionPromise
+
   const collections = await connection.db.collections()
   for (const collection of collections) {
     await collection.deleteMany({})
