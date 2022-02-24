226860cd24351635cbfda71b207f0e67e2e5efa2
Nicolás López <nicolaslopezj@me.com>
2022-02-24 17:41:55 -0300

    dont log mail_url env not set

--- packages/mailing/src/sendEmail/transporter.ts
+++ packages/mailing/src/sendEmail/transporter.ts
@@ -3,7 +3,9 @@ import nodemailer, {Transporter} from 'nodemailer'
 let transporter: Transporter<any> = {} as Transporter<any>
 
 if (!process.env.MAIL_URL) {
-  console.info('MAIL_URL env var not set. Will log emails to console instead.')
+  if (!process.env.ORION_TEST) {
+    console.info('MAIL_URL env var not set. Will log emails to console instead.')
+  }
 } else {
   const currTransporter = nodemailer.createTransport(process.env.MAIL_URL)
 
