b7f96a177fbce2911081ab8b8ede7ccb849d7ff5
Nicolás López <nicolaslopezj@me.com>
2022-03-08 07:34:34 -0300

    adds a comment

--- packages/typed-model/src/decorators/typedModel.ts
+++ packages/typed-model/src/decorators/typedModel.ts
@@ -5,7 +5,7 @@ export function TypedModel(): ClassDecorator {
   return target => {
     MetadataStorage.addSchemaMetadata({target})
 
-    // @ts-expect-error
+    // @ts-expect-error this is a trick to make it work in resolvers without having to call getModelForClass
     target.getModel = () => getModelForClass(target)
   }
 }
