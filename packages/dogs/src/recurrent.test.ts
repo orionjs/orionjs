c579b0a4f68e3472e6709140c90a38fa14617111
Nicolás López <nicolaslopezj@me.com>
2022-03-12 11:59:11 -0300

    separate tests

--- packages/dogs/src/recurrent.test.ts
+++ packages/dogs/src/recurrent.test.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata'
 import {sleep} from '@orion-js/helpers'
 import {defineJob, scheduleJob, startWorkers} from '.'
 
-describe('Global tests', () => {
+describe('Recurrent tests', () => {
   it('Should run a recurrent job', async () => {
     let ran = false
     const job1 = defineJob({
@@ -49,42 +49,5 @@ describe('Global tests', () => {
     await instance.stop()
 
     expect(count).toBeGreaterThanOrEqual(3)
-  })
-
-  it('Should run an event job', async () => {
-    let count = 0
-    const job3 = defineJob({
-      type: 'event',
-      async resolve(params) {
-        count += params.add
-      }
-    })
-
-    const instance = startWorkers({
-      jobs: {job3},
-      workersCount: 1,
-      pollInterval: 10,
-      cooldownPeriod: 10,
-      logLevel: 'info'
-    })
-
-    expect(count).toBe(0)
-
-    await scheduleJob({
-      name: 'job3',
-      params: {add: 5},
-      runIn: 1
-    })
-
-    await scheduleJob({
-      name: 'job3',
-      params: {add: 25},
-      runIn: 1
-    })
-
-    await sleep(100)
-    await instance.stop()
-
-    expect(count).toBe(30)
   })
 })
