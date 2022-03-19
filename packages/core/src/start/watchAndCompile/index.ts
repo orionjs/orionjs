a01b3ab9711caea5cd806c16edd92fde7f874919
Nicolás López <nicolaslopezj@me.com>
2022-03-19 12:06:55 -0300

    clean directory and watches deletes on cli

--- packages/core/src/start/watchAndCompile/index.ts
+++ packages/core/src/start/watchAndCompile/index.ts
@@ -1,8 +1,12 @@
 import ts from 'typescript'
 import {Runner} from '../runner'
+import cleanDirectory from './cleanDirectory'
 import {getHost} from './getHost'
+import {watchDeletes} from './watchDeletes'
 
 export default async function watchAndCompile(runner: Runner) {
+  await cleanDirectory()
   const host = getHost(runner)
   ts.createWatchProgram(host)
+  watchDeletes()
 }
