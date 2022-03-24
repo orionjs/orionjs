e78c0f845bd6c0005845951909d003762320a576
Nicolás López <nicolaslopezj@me.com>
2022-03-24 20:23:35 -0300

    use internal env in mongo url

--- packages/mongodb/src/connect/getMongoURLFromEnv.ts
+++ packages/mongodb/src/connect/getMongoURLFromEnv.ts
@@ -1,18 +1,20 @@
 import {connections} from './connections'
+import {internalGetEnv} from '@orion-js/helpers'
 
 export const getMongoURLFromEnv = (connectionName: string): string => {
   if (connectionName === 'main') {
-    if (!process.env.MONGO_URL) {
+    if (!internalGetEnv('mongo_url', 'MONGO_URL')) {
       throw new Error('MONGO_URL is required')
     }
-    return process.env.MONGO_URL
+    return internalGetEnv('mongo_url', 'MONGO_URL')
   }
 
-  const name = `MONGO_URL_${connectionName.toUpperCase()}`
-  const uri = connections[connectionName]?.uri ?? process.env[name]
+  const envName = `mongo_url_${connectionName.toLowerCase()}`
+  const processEnvName = `MONGO_URL_${connectionName.toUpperCase()}`
+  const uri = connections[connectionName]?.uri ?? internalGetEnv(envName, processEnvName)
   if (!uri) {
     throw new Error(
-      `To use the connection "${connectionName}" you must initialize it first calling getMongoConnection({name: "${connectionName}", uri: "MONGOURI"}) or setting the environment variable ${name}.`
+      `To use the connection "${connectionName}" you must initialize it first calling getMongoConnection({name: "${connectionName}", uri: "MONGOURI"}) or setting the environment variable ${processEnvName}.`
     )
   }
 
