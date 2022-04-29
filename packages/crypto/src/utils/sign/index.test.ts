9a6ab60271490664a53f6871abb5001d128ff3ec
Nicolás López <nicolaslopezj@me.com>
2022-04-29 13:12:09 -0400

    add crpyto utility sign

---
+++ packages/crypto/src/utils/sign/index.test.ts
@@ -0,0 +1,30 @@
+import {sign} from '.'
+
+describe('Sign', () => {
+  it('Should create digests', () => {
+    const payload = 'Hello world'
+    const secret = 'secret'
+    const digest = '0d5548fb7450e619b0753725068707519ed41cd212b0500bc20427e3ef66e08e'
+    expect(sign.sign(payload, secret)).toEqual(digest)
+  })
+
+  it('Should create equally digests for same payloads', () => {
+    const payload = 'Hello world'
+    const secret = 'secret'
+    expect(sign.sign(payload, secret)).toEqual(sign.sign(payload, secret))
+  })
+
+  it('Verify should return true if checksum is correct', () => {
+    const payload = 'Hello world'
+    const secret = 'secret'
+    const checksum = '0d5548fb7450e619b0753725068707519ed41cd212b0500bc20427e3ef66e08e'
+    expect(sign.verify(payload, checksum, secret)).toBe(true)
+  })
+
+  it('Verify should return false if checksum is incorrect', () => {
+    const payload = 'Hello world'
+    const secret = 'secret'
+    const checksum = 'incorrect'
+    expect(sign.verify(payload, checksum, secret)).toBe(false)
+  })
+})
