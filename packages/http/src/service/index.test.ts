b6e612c5da296ea15d43ff0838543c7756e20e7f
Nicolás López <nicolaslopezj@me.com>
2022-04-29 13:11:52 -0400

    improves a http test

--- packages/http/src/service/index.test.ts
+++ packages/http/src/service/index.test.ts
@@ -1,26 +1,40 @@
+import {Inject, Service} from '@orion-js/services'
 import {getServiceRoutes, Route, Routes} from '.'
-import {RouteResponse} from '../types'
+import {getApp} from '../start'
+import request from 'supertest'
+import registerRoutes from '../routes/registerRoutes'
+import {Request} from '../types'
 
 describe('Routes with service injections', () => {
   it('Should define a routes map using services', async () => {
+    @Service()
+    class ServiceExample {
+      sayHi(name: string) {
+        return `hello ${name}`
+      }
+    }
+
     @Routes()
     class RoutesService {
-      @Route({method: 'get', path: '/route1'})
-      async route1() {
+      @Inject()
+      serviceExample: ServiceExample
+
+      @Route({method: 'post', path: '/route-service-test', bodyParser: 'json'})
+      async route(req: Request) {
         return {
           statusCode: 200,
-          body: 'route1'
+          body: {
+            message: this.serviceExample.sayHi(req.body.name)
+          }
         }
       }
     }
 
     const routes = getServiceRoutes(RoutesService)
+    registerRoutes(routes)
 
-    expect(routes).toMatchObject({
-      route1: {
-        method: 'get',
-        path: '/route1'
-      }
-    })
+    const app = getApp()
+    const response = await request(app).post('/route-service-test').send({name: 'nico'})
+    expect(response.body).toEqual({message: `hello nico`})
   })
 })
