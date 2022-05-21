6b41a46c693de172dc398954e0f7034bad00f1c6
Nicolás López <nicolaslopezj@me.com>
2022-05-20 23:35:14 -0400

    allow passing url as service name

--- packages/echoes/src/request/getURL.ts
+++ packages/echoes/src/request/getURL.ts
@@ -1,7 +1,9 @@
 import config from '../config'
 
 export default function (serviceName: string): string {
-  const url = config.requests.services[serviceName]
+  if (serviceName.startsWith('http')) return serviceName
+
+  const url = config?.requests?.services[serviceName]
 
   if (!url) {
     throw new Error(`No URL found in echoes config for service ${serviceName}`)
