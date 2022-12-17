6b992aac777cec07c9a65f0e5905355ac0a1011d
Nicolás López <nicolaslopezj@me.com>
2022-12-17 10:10:59 -0300

    simulate latency in http

--- packages/http/src/routes/executeRequest.ts
+++ packages/http/src/routes/executeRequest.ts
@@ -2,13 +2,24 @@ import {RouteType} from './../types'
 import {onError} from '../errors'
 import {getViewer} from './../viewer'
 import express from 'express'
-import {isNil, isPlainObject} from 'lodash'
+import {isNil, isPlainObject, random} from 'lodash'
+import {internalGetEnv} from '@orion-js/env'
+import {sleep} from '@orion-js/helpers'
+
+const simulateLatency = internalGetEnv('simulate_latency', 'SIMULATE_LATENCY')
 
 export async function executeRequest(
   route: RouteType,
   req: express.Request,
   res: express.Response
 ) {
+  if (simulateLatency) {
+    const time = parseInt(simulateLatency)
+    if (time) {
+      await sleep(random(time * 0.9, time * 1.1))
+    }
+  }
+
   try {
     const viewer = await getViewer(req)
     const result = await route.resolve(req, res, viewer)
