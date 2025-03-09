f46eed5277bfbdf7c6e4eb39f78c18d8598e1c5f
Nicolás López <nicolaslopezj@me.com>
2025-03-09 02:30:55 -0300

    mcp version

--- packages/core/src/start/copyMCP/consts.ts
+++ packages/core/src/start/copyMCP/consts.ts
@@ -1,3 +1,3 @@
 // Define current MCP version - update this when a new version needs to be deployed
-export const MCP_VERSION = 'v2'
+export const MCP_VERSION = 'v1'
 export const VERSION_FILE = 'version.txt'
