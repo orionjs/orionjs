b8f6269c15808077fdd83e64024f8c5cb1811b70
Nicolás López <nicolaslopezj@me.com>
2025-03-10 14:56:42 -0300

    fixes more things

--- example/app/exampleComponent/controllers/jobs/ExampleJobs/index.ts
+++ example/app/exampleComponent/controllers/jobs/ExampleJobs/index.ts
@@ -2,14 +2,15 @@ import {Jobs, RecurrentJob} from '@orion-js/dogs'
 import {logger} from '@orion-js/logger'
 import {Inject} from '@orion-js/services'
 import {ExampleService} from 'app/exampleComponent/services/ExampleService'
+import {type ExampleService as ExampleServiceType} from 'app/exampleComponent/services/ExampleService'
 
 @Jobs()
 export default class ExampleJobs {
-  @Inject()
-  private exampleService: ExampleService
+  @Inject(() => ExampleService)
+  private exampleService: ExampleServiceType
 
   @RecurrentJob({
-    runEvery: 1000 * 60,
+    runEvery: 1000 * 10,
   })
   async createExamples() {
     logger.info('Creating example...')
