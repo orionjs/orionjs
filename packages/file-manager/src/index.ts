da3ee610943d3479ea4f451493ba805f0c66985a
Nicolás López <nicolaslopezj@me.com>
2025-03-17 19:17:42 -0300

    add FileManagerOptions to exports in index.ts

--- packages/file-manager/src/index.ts
+++ packages/file-manager/src/index.ts
@@ -1,7 +1,15 @@
 import resolvers from './resolvers'
-import {setupFileManager, getAWSCredentials} from './credentials'
+import {setupFileManager, getAWSCredentials, FileManagerOptions} from './credentials'
 import {Files} from './Files'
 import {getFileURL} from './File/resolvers/url'
 import {FileSchema} from './File/schema'
 
-export {resolvers, setupFileManager, getAWSCredentials, Files, getFileURL, FileSchema}
+export {
+  resolvers,
+  FileManagerOptions,
+  setupFileManager,
+  getAWSCredentials,
+  Files,
+  getFileURL,
+  FileSchema,
+}
